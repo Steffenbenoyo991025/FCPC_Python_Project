5be4c29c758bdecdaeb8e5a6dca074d0a2ff372f
Steffenbenoyo991025 <45374976+Steffenbenoyo991025@users.noreply.github.com>
2018-11-27 07:53:51 +0800

    Create Sample_BENOYO.py

---
+++ Sample_BENOYO.py
@@ -0,0 +1,39 @@
+# Decalaring and Initialising Variables in Python
+
+#String
+Honda = "Fboi"
+Toyota = "Lover"
+Mazda = "Grade11"
+Mitsubishi = "Grade7"
+Hyundai = "Mom"
+
+print (Honda)
+print (Toyota)
+print (Mazda)
+print (Mitsubishi)
+print (Hyundai)
+
+
+# Arithmetic
+addition = 1 + 1
+subtraction = 1 - 1
+division = 1/1
+multiplication = 1 * 1
+modulus = 5% 3
+
+print (addition)
+print (subtraction)
+print (division)
+print (multiplication)
+print (modulus)
+
+if addition> division:
+    print ("Hello")
+    print ("Its")
+    print ("Me")
+    print ("Pogi")
+elif modulus> multiplication:
+    print ("Kowloon")
+    print ("Colon")
+else:
+    print ("Eow po")
