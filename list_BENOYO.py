f2b8902cf5913c086a96c86bb1cb8c37b46afdd4
Steffenbenoyo991025 <45374976+Steffenbenoyo991025@users.noreply.github.com>
2018-11-27 10:01:35 +0800

    Create list_BENOYO.py

---
+++ list_BENOYO.py
@@ -0,0 +1,18 @@
+"LISTS"
+
+Car_Brands = ["Honda","Toyota","Mazda"]
+Numbers = [1,2,3,4,5,6,7,8,9,10]
+Decimal_Points = [1.0,1.2,1.3,1.4,1.5]
+
+for x in range(len(Car_Brands)):
+    print(Car_Brands[x])
+
+for y in range(len(Numbers)):
+    print(Numbers[y])
+
+for z in range(len(Decimal_Points)):
+    print(Decimal_Points[z])
+
+print(len(Car_Brands))
+print(len(Numbers))
+print(len(Decimal_Points))
